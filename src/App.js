518d5530e6a2281b45f67e5b7eb8c049b769146b
Beth Damato <bethadamato@gmail.com>
2019-09-10 23:03:33 -0400

    initial commit of project files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import AppBar from '@material-ui/core/AppBar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './styles/styles.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <AppBar position='static'>
+          <img className='logo' src={require('./images/calogo.jpeg')} alt='logo ' />
+        </AppBar>
+
+      </div>
+    )
+  }
 }
 
-export default App;
+export default connect()(App);
